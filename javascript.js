9e5238f78c943dc1b7221fd9ca3bff781b0ee75f
Michele Farias <mi.farias182@gmail.com>
2021-05-19 15:01:14 -0300

    create of change  color gri elements function

--- javascript.js
+++ javascript.js
@@ -1,20 +1,48 @@
-function createDivs(gridLen){
-    const container = document.getElementById('container');
+function createGrid(gridLen){
+    let container = document.getElementById('container');
     container.style.setProperty('--grid-length', gridLen);
-
+    
     for(var i=0;i<(gridLen * gridLen);i++){
-        const div = document.createElement('div');
-        div.setAttribute('class','divGrid');
-        container.appendChild(div);
+        const gridElement = document.createElement('div');
+        gridElement.setAttribute('class','divGrid');
+        container.appendChild(gridElement);
+        gridElement.addEventListener('mouseover',colorGen);       
+        
+    }
+    
+  
+}
+
+  
+function removeGrid(){
+    const removeGridElements = document.getElementsByClassName('divGrid');
+      while (removeGridElements[0]) {
+        removeGridElements[0].parentNode.removeChild(removeGridElements[0]);
     }
 }
 
-createDivs(100);
-var randomColor = '#'+ Math.floor(Math.random()*16777215).toString(16);
-const divGrid = document.querySelectorAll('.divGrid').forEach((divGrid) => {
+function clear(){
+    const gridElements = document.getElementsByClassName('divGrid');
+    gridElements
+    //if()
 
-	divGrid.addEventListener('mouseover',() =>{
-		divGrid.style.backgroundColor = randomColor;
-	});
+}
 
-});
+function colorGen(e) {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+  
+
+function setGrid(e){   
+   let gridLength= e.target.value;
+   removeGrid();
+   createGrid(gridLength);
+   
+}
+
+     createGrid(50);
+     document.getElementById('range-grid').addEventListener('change',setGrid);
+     //document.getElementById('clear').addEventListener('click';
